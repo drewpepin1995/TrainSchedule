39a0d2f9269f5a5cebb1c401bf37ea119f2fd63c
Drew Pepin <drewpepin1995@gmail.com>
2019-08-10 21:29:50 -0400

    Setting up firebase database/pushing data on click

---
+++ assets/javascript/app.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+    let firebaseConfig = {
+        apiKey: "AIzaSyCuvwZjCDz2FzvKfmyEhkCSDf_zmZvFSWQ",
+        authDomain: "train-schedule-40081.firebaseapp.com",
+        databaseURL: "https://train-schedule-40081.firebaseio.com/",
+        projectId: "train-schedule-40081",
+        storageBucket: "train-schedule-40081.appspot.com",
+        messagingSenderId: "861788756312",
+        appId: "1:861788756312:web:cf3114dd0a12c914"
+    };
+
+
+    firebase.initializeApp(firebaseConfig);
+
+    let database = firebase.database();
+
+
+
+    let trainRef = database.ref("/trainSchedule")
+
+    trainRef.on("value", function (snap) {
+        trainName = snap.val().trainName;
+        trainDestination = snap.val().trainDestination;
+        trainTime = snap.val().trainTime;
+        trainFrequency = snap.val().trainFrequency;
+
+    });
+
+
+    $("#submitBtn").on("click", function (event) {
+        event.preventDefault()
+
+        let trainName = $("#trainName").val().trim();
+        let trainDestination = $("#trainDestination").val().trim();
+        let trainTime = $("#trainTime").val().trim();
+        let trainFrequency = $("#trainFrequency").val().trim();
+
+        alert("New train added!")
+
+        $("#trainName").val("")
+        $("#trainDestination").val("")
+        $("#trainTime").val("")
+        $("#trainFrequency").val("")
+
+
+
+
+        trainRef.push({
+            Name: trainName,
+            Destination: trainDestination,
+            Time: trainTime,
+            Frequency: trainFrequency,
+        });
+    });
+
+    trainRef.on("child_added", function (childSnapshot) {
+        const data = childSnapshot.val();
+
+        console.log(childSnapshot.val().Name);
+        console.log(childSnapshot.val().Time);
+        console.log(childSnapshot.val().Destination);
+        console.log(childSnapshot.val().Frequency);
+
+
+    });
+});
+
